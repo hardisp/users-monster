bdb106af8d067bc9e947dd90742ceb3549c5df89
Hardi Suputra <jiksdi12@gmail.com>
2020-01-19 13:18:12 +0800

    Updates

---
+++ precache-manifest.c3a91ae5fb68361c71718437adeb4f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796664c9e78d5ef58ead4524af93ab57",
+    "url": "/users-monster/index.html"
+  },
+  {
+    "revision": "60d33792b422d3492b77",
+    "url": "/users-monster/static/css/main.3ee293ea.chunk.css"
+  },
+  {
+    "revision": "4fb85e1eb12a13737b1e",
+    "url": "/users-monster/static/js/2.b20fde1d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/users-monster/static/js/2.b20fde1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60d33792b422d3492b77",
+    "url": "/users-monster/static/js/main.99bf63fd.chunk.js"
+  },
+  {
+    "revision": "83d61b51e653aff919c3",
+    "url": "/users-monster/static/js/runtime-main.e1ac6333.js"
+  }
+]);
